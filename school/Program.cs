638c226474d1aa900e598d42c4c262fbbe6d1e13
Артур <ostrikovartur21@gmail.com>
2022-05-01 17:58:27 +0300

    Add project files.

---
+++ school/Program.cs
@@ -0,0 +1,113 @@
+﻿using School;
+Address malynivka = new()
+{
+    Country = "Ukraine",
+    City = "Malynivka",
+    Street = "Вул. Шкільна 1",
+    PostalCode = 22360
+};
+
+Employee director = new()
+{
+    FirstName = "Володимир",
+    LastName = "Сиченко"
+};
+
+School.School malynivskaSchool = new()
+{
+    Address = malynivka,
+    Name = "Малинівська школа #1",
+    OpeningDate = new DateOnly(2002, 1, 1),
+    Floors = new List<Floor>(),
+    Rooms = new List<Room>(),
+    Director = director,
+    Employees = new List<Employee>()
+};
+
+Floor firstFloor = new()
+{
+    Number = 1,
+};
+
+malynivskaSchool.Floors.Add(firstFloor);
+
+Floor secondFloor = new()
+{
+    Number = 2,
+};
+malynivskaSchool.Floors.Add(secondFloor);
+
+Floor thirdFloor = new()
+{
+    Number = 3,
+};
+malynivskaSchool.Floors.Add(thirdFloor);
+
+Room mathRoom = new()
+{
+    Number = 101,
+    Type = RoomType.Regular | RoomType.Math,
+    Floor = firstFloor
+};
+
+Room bioRoom = new()
+{
+    Number = 202,
+    Type = RoomType.Regular | RoomType.Biology,
+    Floor = secondFloor
+};
+Room informRoom = new()
+{
+    Number = 103,
+    Type = RoomType.Regular | RoomType.Informatic,
+    Floor = firstFloor
+};
+Room literatureRoom = new()
+{
+    Number = 204,
+    Type = RoomType.Regular | RoomType.Literature,
+    Floor = secondFloor
+};
+Room gymRoom = new()
+{
+    Number = 100,
+    Type = RoomType.Regular | RoomType.Gym,
+    Floor = thirdFloor
+};
+Room physicsRoom = new()
+{
+    Number = 203,
+    Type = RoomType.Physics,
+    Floor = secondFloor
+};
+Room hallRoom = new()
+{
+    Number = 200,
+    Type = RoomType.Hall,
+    Floor = secondFloor
+};
+
+
+malynivskaSchool.Rooms.Add(mathRoom);
+malynivskaSchool.Rooms.Add(bioRoom);
+malynivskaSchool.Rooms.Add(informRoom);
+malynivskaSchool.Rooms.Add(literatureRoom);
+malynivskaSchool.Rooms.Add(gymRoom);
+malynivskaSchool.Rooms.Add(physicsRoom);
+malynivskaSchool.Rooms.Add(hallRoom);
+
+firstFloor.AddRoom(mathRoom);
+
+firstFloor.AddRoom(informRoom);
+
+secondFloor.AddRoom(bioRoom);
+
+secondFloor.AddRoom(literatureRoom);
+
+thirdFloor.AddRoom(gymRoom);
+
+secondFloor.AddRoom(physicsRoom);
+
+secondFloor.AddRoom(hallRoom);
+
+malynivskaSchool.Print();
---
+++ school/School.cs
@@ -0,0 +1,66 @@
+﻿namespace School
+{
+    class School
+    {
+        public Address Address { get; set; }
+        public ICollection<Room> Rooms { get; set; }
+        public DateOnly OpeningDate { get; set; }
+        public string Name { get; set; }
+        public ICollection<Floor> Floors { get; set; }
+        public Employee Director { get; set; }
+        public ICollection<Employee> Employees { get; set; }
+        public void Print()
+        {
+            Console.WriteLine($"Name {Name}");
+            Console.WriteLine($"Number of rooms on first floor {Rooms.Count}");
+            for (int i = 0; i < Floors.Count; i++)
+            {
+                
+            }
+        }
+    }
+    class Address
+    {
+        public string Country { get; set; }
+        public string City { get; set; }
+        public string Street { get; set; }
+        public int PostalCode { get; set; }
+    }
+    class Employee
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+    }
+    class Room
+    {
+        public int Number { get; set; }
+        public RoomType Type { get; set; }
+        public Floor Floor { get; set; }
+    }
+    class Floor
+    {
+        public int Number { get; set; }
+        private List<Room> _rooms;
+        public IEnumerable<Room> Rooms => _rooms;
+        public Floor()
+        {
+            _rooms = new List<Room>();
+        }
+        public void AddRoom(Room room)
+        {
+            _rooms.Add(room);
+        }
+    }
+    [Flags]
+    enum RoomType
+    {
+        Regular = 1,
+        Math = 2,
+        Biology = 4,
+        Literature = 8,
+        Informatic = 16,
+        Gym = 32,
+        Physics = 64,
+        Hall = 128,
+    }
+}
