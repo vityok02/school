4741ee1a32770cd9908f3868b1323954a10d6f9d
Віктор Липкань <lipkan02@gmail.com>
2022-09-25 10:57:55 +0300

    DB

---
+++ schoolDB.request.sql
@@ -0,0 +1,11 @@
+use schoolBD
+
+select * from School
+select * from Address
+select * from Floor
+select * from Room
+select * from RoomType
+select * from Employee
+select * from Director
+select * from Teacher
+select * from SchoolEmployee
