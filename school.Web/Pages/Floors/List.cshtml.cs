28d6aa4089b408a7431f74e68b86d3a00309d4a6
Віктор Липкань <lipkan02@gmail.com>
2022-12-13 22:44:52 +0200

    Added the ability to delete floors

--- school.Web/Pages/Floors/List.cshtml.cs
+++ school.Web/Pages/Floors/List.cshtml.cs
@@ -4,48 +4,75 @@ using SchoolManagement.Data;
 using SchoolManagement.Models;
 using SchoolManagement.Models.Interfaces;
 
-namespace SchoolManagement.Web.Pages.Floors
+namespace SchoolManagement.Web.Pages.Floors;
+
+public class FloorListModel : PageModel
 {
-    public class ListModel : PageModel
+    AppDbContext _ctx;
+    private readonly IRepository<Floor>? _floorRepository;
+    private readonly IRepository<School> _schoolRepository;
+
+    public FloorListModel(IRepository<Floor>? floorRepository, IRepository<School> schoolRepository, AppDbContext ctx)
     {
-        AppDbContext _ctx;
-        private readonly IRepository<Floor>? _floorRepository;
-        private readonly IRepository<School> _schoolRepository;
-
-        public ListModel(IRepository<Floor>? floorRepository, IRepository<School> schoolRepository, AppDbContext ctx)
-        {
-            _floorRepository = floorRepository;
-            _schoolRepository = schoolRepository;
-            _ctx = ctx;
-            Floors = _ctx.Floors.Where(f=>f.SchoolId == SchoolId);
-        }
-        public School School { get; set; }
-        public IEnumerable<School> Schools { get; set; }
-        public IEnumerable<Floor> Floors { get; set; }
-
-        public int SchoolId { get; set; }
-
-
-        public void OnGet()
-        {
-            SchoolId = int.Parse(HttpContext.Request.Cookies["SchoolId"]);
-            Floors = _floorRepository.GetAll(f => f.SchoolId == SchoolId);
-        }
-
-        public void OnPostAddFloor()
-        {
-            SchoolId = int.Parse(HttpContext.Request.Cookies["SchoolId"]);
-            School = _schoolRepository.Get(SchoolId);
-            int number;
-            if (Floors is null)
-            {
-                number = 0;
-            }
-            else
-                number = Floors.Last().Number;
-            Floor floor = new(number + 1);
-            var (valid, error) = School.AddFloor(floor);
-            _ctx.SaveChanges();
-        }
+        _floorRepository = floorRepository;
+        _schoolRepository = schoolRepository;
+        _ctx = ctx;
+        Floors = _ctx.Floors.Where(f=>f.SchoolId == SchoolId);
     }
+    public School School { get; set; }
+    public IEnumerable<School> Schools { get; set; }
+    public static IEnumerable<Floor> Floors { get; private set; }
+
+    public int SchoolId { get; set; }
+
+
+    public void OnGet()
+    {
+        Floors = _floorRepository.GetAll(f => f.SchoolId == SchoolId);
+        SchoolId = int.Parse(HttpContext.Request.Cookies["SchoolId"]);
+    }
+
+    public void OnPost()
+    {
+
+    }
+
+    public void OnPostAddFloor()
+    {
+        SchoolId = int.Parse(HttpContext.Request.Cookies["SchoolId"]);
+        School = _schoolRepository.Get(SchoolId);
+        int number;
+
+        if (!Floors.Any())
+            number = 0;
+        else
+            number = Floors.Last().Number;
+
+        Floor floor = new(number + 1);
+        //_floorRepository!.Add(floor); ?
+        var (valid, error) = School.AddFloor(floor);
+        _ctx.SaveChanges();
+    }
+    public void OnPostAddBasement()
+    {
+        SchoolId = int.Parse(HttpContext.Request.Cookies["SchoolId"]);
+        School = _schoolRepository.Get(SchoolId);
+        int number;
+        if (!Floors.Any())
+            number = 0;
+        else
+            number = Floors.First().Number;
+
+        Floor floor = new(number - 1);
+        var (valid, error) = School!.AddFloor(floor);
+        _ctx.SaveChanges();
+    }
+    //public IActionResult OnPostDelete(int id)
+    //{
+    //    var floor = _floorRepository.Get(id);
+    //    //_ctx.Remove(floor);
+    //    //_ctx.SaveChanges();
+    //    _floorRepository.Delete(floor!);
+    //    return RedirectToPage("List");
+    //}
 }
