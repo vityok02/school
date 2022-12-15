3de1accb6c733ccdeb6a90b188ee783c553b804e
Віктор Липкань <lipkan02@gmail.com>
2022-12-15 22:57:12 +0200

    Adding room page

---
+++ school.Web/Pages/Rooms/List.cshtml.cs
@@ -0,0 +1,27 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using SchoolManagement.Models;
+using SchoolManagement.Models.Interfaces;
+
+namespace SchoolManagement.Web.Pages.Rooms
+{
+    public class ListModel : PageModel
+    {
+        private readonly IRepository<Room> _roomRepository;
+        private readonly IRepository<Floor> _floorRepository;
+        public ListModel(IRepository<Room> roomRepository, IRepository<Floor> floorRepository)
+        {
+            _roomRepository = roomRepository;
+            _floorRepository = floorRepository;
+        }
+        public static IEnumerable<Room> Rooms { get; private set; }
+        public int SchoolId { get; set; }
+        public IEnumerable<Floor> Floors { get; set; }
+        public void OnGet()
+        {
+            SchoolId = int.Parse(HttpContext.Request.Cookies["SchoolId"]);
+            Floors = _floorRepository.GetAll(f => f.SchoolId == SchoolId);
+            Rooms = _roomRepository.GetAll(r => r.Floor.SchoolId == SchoolId);
+        }
+    }
+}
