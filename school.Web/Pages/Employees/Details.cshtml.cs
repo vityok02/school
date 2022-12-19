73ed5814eba56daec037f6812c34ea559d8b04e9
Віктор Липкань <lipkan02@gmail.com>
2022-12-19 21:40:11 +0200

    Updated

--- school.Web/Pages/Employees/Details.cshtml.cs
+++ school.Web/Pages/Employees/Details.cshtml.cs
@@ -3,26 +3,26 @@ using Microsoft.AspNetCore.Mvc.RazorPages;
 using SchoolManagement.Models;
 using SchoolManagement.Models.Interfaces;
 
-namespace SchoolManagement.Web.Pages.Employees
+namespace SchoolManagement.Web.Pages.Employees;
+
+public class DetailsModel : PageModel
 {
-    public class DetailsModel : PageModel
+    private readonly IRepository<Employee> _employeeRepository;
+    public Employee? Employee { get; set; }
+
+    public DetailsModel(IRepository<Employee> employeeRepository)
     {
-        private readonly IRepository<Employee> _employeeRepository;
-        public Employee? Employee { get; set; }
-        public DetailsModel(IRepository<Employee> employeeRepository)
-        {
-            _employeeRepository = employeeRepository;
-        }
+        _employeeRepository = employeeRepository;
+    }
 
-        public IActionResult OnGet(int id)
-        {
-            Employee = _employeeRepository.Get(id);
+    public IActionResult OnGet(int id)
+    {
+        Employee = _employeeRepository.Get(id);
 
-            if (Employee == null)
-            {
-                return NotFound("Employee not found");
-            }
-            return Page();
+        if (Employee == null)
+        {
+            return NotFound("Employee not found");
         }
+        return Page();
     }
 }
--- school.Web/Pages/Employees/Edit.cshtml.cs
+++ school.Web/Pages/Employees/Edit.cshtml.cs
@@ -3,59 +3,58 @@ using Microsoft.AspNetCore.Mvc.RazorPages;
 using SchoolManagement.Models;
 using SchoolManagement.Models.Interfaces;
 
-namespace SchoolManagement.Web.Pages.Employees
+namespace SchoolManagement.Web.Pages.Employees;
+
+public class EditModel : PageModel
 {
-    public class EditModel : PageModel
+    private readonly IRepository<Employee> _employeeRepository;
+    public Employee? Employee { get; set; }
+    public string Message { get; set; } = "";
+    public EditModel(IRepository<Employee> employeeRepository)
     {
-        private readonly IRepository<Employee> _employeeRepository;
-        public Employee? Employee { get; set; }
-        public string Message { get; set; } = "";
-        public EditModel(IRepository<Employee> employeeRepository)
+        _employeeRepository = employeeRepository;
+    }
+
+    public IActionResult OnGet(int id)
+    {
+        Employee = _employeeRepository.Get(id)!;
+
+        if (Employee is null)
         {
-            _employeeRepository = employeeRepository;
+            return NotFound("Employee not found");
         }
 
-        public IActionResult OnGet(int id)
+        return Page();
+    }
+
+    public IActionResult OnPost(int id, string firstName, string lastName, int age)
+    {
+        var sId = HttpContext.Request.Cookies["SchoolId"];
+        if (sId is null || !int.TryParse(sId, out int schoolId))
         {
-            Employee = _employeeRepository.Get(id)!;
-
-            if (Employee is null)
-            {
-                return NotFound("Employee not found");
-            }
-
-            return Page();
+            return NotFound("School not found");
         }
 
-        public IActionResult OnPost(int id, string firstName, string lastName, int age)
+        var employees = _employeeRepository.GetAll(e => e.SchoolId == schoolId);
+
+        var employee = _employeeRepository.Get(id);
+
+        if (employee is null) 
         {
-            var sId = HttpContext.Request.Cookies["SchoolId"];
-            if (sId is null || !int.TryParse(sId, out int schoolId))
-            {
-                return NotFound("School not found");
-            }
-
-            var employees = _employeeRepository.GetAll(e => e.SchoolId == schoolId);
-
-            var employee = _employeeRepository.Get(id);
-
-            if (employee is null) 
-            {
-                return NotFound("Employee is not found");
-            }
-
-            //if(employees.Any(emp => emp.FirstName == firstName 
-            //    && emp.LastName == lastName 
-            //    && emp.Age == age))
-            //{
-            //    Message = "Such employee already exist";
-            //    return Page();
-            //}
-
-            employee.UpdateInfo(firstName, lastName, age);
-
-            _employeeRepository.Update(employee);
-            return Redirect($"/employees/{id}");
+            return NotFound("Employee is not found");
         }
+
+        //if(employees.Any(emp => emp.FirstName == firstName 
+        //    && emp.LastName == lastName 
+        //    && emp.Age == age))
+        //{
+        //    Message = "Such employee already exist";
+        //    return Page();
+        //}
+
+        employee.UpdateInfo(firstName, lastName, age);
+
+        _employeeRepository.Update(employee);
+        return Redirect($"/employees/{id}");
     }
 }
