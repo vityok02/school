d5d0c4ebeac1b13a1fe6c1d5593f1522e7620288
Віктор Липкань <lipkan02@gmail.com>
2023-02-19 15:45:25 +0200

    Added employee repository

---
+++ school.Data/EmployeeRepository.cs
@@ -0,0 +1,42 @@
+﻿using SchoolManagement.Models;
+using SchoolManagement.Models.Interfaces;
+using System.Linq.Expressions;
+
+namespace SchoolManagement.Data;
+
+public class EmployeeRepository : Repository<Employee>, IEmployeeRepository
+{
+    private readonly AppDbContext _dbContext;
+
+    public EmployeeRepository(AppDbContext dbContext)
+        : base(dbContext)
+    {
+        _dbContext = dbContext;
+    }
+
+    public IEnumerable<Employee> GetSchoolEmployees(int schoolId, string filterByName = null, int filterByAge = 0, string filterByJob = null)
+    {
+        bool filters<T>(T e) where T : Employee
+        {
+            return (string.IsNullOrEmpty(filterByName) || e.FirstName.Contains(filterByName))
+                && (string.IsNullOrEmpty(filterByJob) || e.Job.Contains(filterByJob))
+                && (filterByAge == 0 || e.Age == filterByAge);
+        }
+
+        var directors = _dbContext.Set<Director>()
+            .Where(d => d.SchoolId == schoolId)
+            .Where(filters)
+            .ToArray();
+
+        var teachers = _dbContext.Set<Teacher>()
+            .Where(d => d.SchoolId == schoolId)
+            .Where(filters)
+            .ToArray();
+
+        var employees = new List<Employee>();
+        employees.AddRange(directors);
+        employees.AddRange(teachers);
+
+        return employees;
+    }
+}
