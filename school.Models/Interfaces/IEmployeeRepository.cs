f48f48e264bb3f850a2631f429524a98af5b76c1
Віктор Липкань <lipkan02@gmail.com>
2023-02-19 15:43:58 +0200

    Added interface

---
+++ school.Models/Interfaces/IEmployeeRepository.cs
@@ -0,0 +1,6 @@
+﻿namespace SchoolManagement.Models.Interfaces;
+
+public interface IEmployeeRepository : IRepository<Employee>
+{
+    IEnumerable<Employee> GetSchoolEmployees(int schoolId, string filterByName, int filterByAge, string filterByJob);
+}
