a954a231a6d83d3dfa4ecd7da74a9577ae988792
Віктор Липкань <lipkan02@gmail.com>
2023-03-26 15:04:33 +0300

    added position edit page

---
+++ school.Web/Pages/Positions/Edit.cshtml.cs
@@ -0,0 +1,44 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore.Query.Internal;
+using SchoolManagement.Models;
+using SchoolManagement.Models.Interfaces;
+
+namespace SchoolManagement.Web.Pages.Positions
+{
+    public class EditModel : BasePageModel
+    {
+        private readonly IPositionRepository _positionRepository;
+
+        public Position Position { get; set; } = null!;
+
+        public EditModel(IRepository<School> schoolRepository, IPositionRepository positionRepository)
+            : base(schoolRepository)
+        {
+            _positionRepository = positionRepository;
+        }
+
+        public IActionResult OnGet(int id)
+        {
+            Position = _positionRepository.Get(id);
+            if (Position == null)
+            {
+                return RedirectToPage("AllPositions");
+            }
+            return Page();
+        }
+
+        public IActionResult OnPost(int id, string name)
+        { 
+            var position = _positionRepository.Get(id);
+            if (position == null)
+            {
+                return RedirectToPage("AllPositions");
+            }
+            position!.Name = name;
+            _positionRepository.Update(position);
+
+            return RedirectToPage("AllPositions");
+        }
+    }
+}
