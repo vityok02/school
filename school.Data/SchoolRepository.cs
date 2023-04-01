f81f0a44b1d3573c4acafd1c49803d41c0b82029
Віктор Липкань <lipkan02@gmail.com>
2023-04-01 23:20:15 +0300

    Added school repository

---
+++ school.Data/SchoolRepository.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SchoolManagement.Models;
+using SchoolManagement.Models.Interfaces;
+using System.Linq.Expressions;
+
+namespace SchoolManagement.Data;
+
+public class SchoolRepository : Repository<School>, ISchoolRepository
+{
+    private readonly AppDbContext _dbContext;
+
+    public SchoolRepository(AppDbContext dbContext)
+        :base(dbContext)
+    {
+        _dbContext = dbContext;
+    }
+
+    public IEnumerable<School> GetSchools(Expression<Func<School, bool>> predicate,
+        Func<IQueryable<School>, IOrderedQueryable<School>> orderBy = null!)
+    {
+        var schools = _dbContext
+            .Schools
+            .Include(s => s.Address)
+            .Where(predicate);
+
+        if (orderBy is not null)
+        {
+            schools = orderBy(schools);
+        }
+
+        return schools;
+    }
+}
