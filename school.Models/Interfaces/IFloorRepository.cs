fdbc89758e5b113dd21cf0acd646b65af38aabef
Віктор Липкань <lipkan02@gmail.com>
2023-04-28 20:19:30 +0300

    Added floor repository

---
+++ school.Models/Interfaces/IFloorRepository.cs
@@ -0,0 +1,7 @@
+﻿namespace SchoolManagement.Models.Interfaces;
+
+public interface IFloorRepository : IRepository<Floor>
+{
+    public Floor? GetFloor(int id);
+    public IEnumerable<Floor> GetFloors(int schoolId);
+}
