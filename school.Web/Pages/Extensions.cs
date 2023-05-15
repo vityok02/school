cf0ba6a239f95ec6aedc8795ef9dd0dd2158977e
Віктор Липкань <lipkan02@gmail.com>
2023-05-15 12:39:28 +0300

    Added validator

---
+++ school.Web/Pages/Extensions.cs
@@ -0,0 +1,21 @@
+﻿using FluentValidation.Results;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
+
+namespace SchoolManagement.Web.Pages;
+
+public static class Extensions
+{
+    public static void AddToModelState(
+        this ValidationResult result,
+        ModelStateDictionary modelState,
+        string? propertyPrefix = null)
+    {
+        foreach (var error in result.Errors)
+        {
+            var name = string.IsNullOrWhiteSpace(propertyPrefix)
+                ? error.PropertyName
+                : $"{propertyPrefix}.{error.PropertyName}";
+            modelState.AddModelError(name, error.ErrorMessage);
+        }
+    }
+}
