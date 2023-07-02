a753f11203584688778683922465963d7334a78d
Віктор Липкань <lipkan02@gmail.com>
2023-07-02 10:54:23 +0300

    Removed unnecessary code

--- school.Data/AppDbContext.cs
+++ school.Data/AppDbContext.cs
@@ -18,7 +18,6 @@ public class AppDbContext : DbContext
     public AppDbContext(DbContextOptions<AppDbContext> options)
         : base(options)
     {
-        Database.EnsureCreated();
     }
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
