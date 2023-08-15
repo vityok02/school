83867ac3bb1bf7d3f8f57534efccc01476b9c2e7
Віктор Липкань <lipkan02@gmail.com>
2023-08-15 12:35:09 +0300

    add base list page model

---
+++ school.Web/Pages/BaseListPageModel.cs
@@ -0,0 +1,15 @@
+﻿using SchoolManagement.Models.Interfaces;
+
+namespace SchoolManagement.Web.Pages;
+
+public abstract class BaseListPageModel : BasePageModel
+{
+    public PaginatedList<object> Items { get; protected set; } = default!;
+
+    public abstract string ListPageUrl { get; }
+
+    protected BaseListPageModel(ISchoolRepository schoolRepository)
+        : base(schoolRepository)
+    {
+    }
+}
