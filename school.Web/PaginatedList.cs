00b5f358f4ee78abb0bea949c3d53538609e95a7
Віктор Липкань <lipkan02@gmail.com>
2023-08-15 12:31:48 +0300

    refactor: use constructor for create list

--- school.Web/PaginatedList.cs
+++ school.Web/PaginatedList.cs
@@ -8,22 +8,16 @@ public class PaginatedList<T> : List<T>
     public bool HasNextPage => PageIndex < TotalPages;
     public static int PageSize => 10;
 
-    public PaginatedList(List<T> items, int count, int pageIndex)
+    public PaginatedList(IEnumerable<T> items, int pageIndex)
     {
+        var count = items.Count();
+
         PageIndex = pageIndex;
         TotalPages = (int)Math.Ceiling(count / (double)PageSize);
 
-        AddRange(items);
-    }
-
-    public static PaginatedList<T> Create(IEnumerable<T> source, int pageIndex)
-    {
-        var count = source.Count();
-        var items = source.Skip(
+        AddRange(items.Skip(
             (pageIndex - 1) * PageSize)
             .Take(PageSize)
-            .ToList();
-
-        return new PaginatedList<T>(items, count, pageIndex);
+            .ToList());
     }
 }
