12551934f3e9b1bde03ca4ac049a005b63943eaa
Abdul4ik02 <lipkan02@gmail.com>
2023-08-24 09:29:15 +0300

    add students endpoinds

---
+++ SchoolManagement.API/Endpoints/StudentsEndpoints.cs
@@ -0,0 +1,16 @@
+﻿using SchoolManagement.Models;
+using SchoolManagement.Models.Interfaces;
+
+namespace SchoolManagement.API.Endpoints;
+
+public static class StudentsEndpoints
+{
+    public static void Map(WebApplication app)
+    {
+        app.MapGet("/schools/{id}/students", async (IRepository<Student> repository, int id) =>
+            await repository.GetAllAsync(s => s.SchoolId == id));
+
+        app.MapGet("/schools/{id}/students/{studentId}", async (IRepository<Student> repository, int studentId) =>
+            await repository.GetAsync(studentId));
+    }
+}
