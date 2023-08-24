77d7bb561259a38b40eb8c92fd08f24971e620e7
Abdul4ik02 <lipkan02@gmail.com>
2023-08-24 09:30:23 +0300

    comment code

---
+++ SchoolManagement.API/Endpoints/RoomsEndpoints.cs
@@ -0,0 +1,21 @@
+﻿using SchoolManagement.Models.Interfaces;
+
+namespace SchoolManagement.API.Endpoints;
+
+public static class RoomsEndpoints
+{
+    public static void Map(WebApplication app)
+    {
+        app.MapGet("/schools/{id}/rooms", async (IRoomRepository repository, int id) =>
+            await repository.GetRoomsAsync(id));
+
+        app.MapGet("/schools/{id}/rooms/{roomId}", async (IRoomRepository repository, int id, int roomId) =>
+            await repository.GetAsync(roomId));
+
+        //app.MapPost("/schools/{id}/rooms", null!);
+
+        //app.MapPut("/schools/{id}/rooms/{roomId}", null!);
+
+        //app.MapDelete("/schools/{id}/rooms/{roomId}", null!);
+    }
+}
