718315c36c857ad39b26336c10fb8818666b349f
Віктор Липкань <lipkan02@gmail.com>
2023-08-29 14:00:42 +0300

    add method (get position for school)

--- school.Models/Interfaces/IPositionRepository.cs
+++ school.Models/Interfaces/IPositionRepository.cs
@@ -6,11 +6,10 @@ public interface IPositionRepository : IRepository<Position>
 {
     Task<IEnumerable<Position>> GetSchoolPositions(int schoolId, Expression<Func<Position, bool>>? predicate = null,
         Func<IQueryable<Position>, IOrderedQueryable<Position>>? orderBy = null);
-
     Task<bool> HasSchoolPositions(int schoolId);
-
     Task<IEnumerable<Position>> GetAllPositions(int schoolId, Expression<Func<Position, bool>>? predicate = null,
         Func<IQueryable<Position>, IOrderedQueryable<Position>>? orderBy = null);
     Task<Position> GetPosition(int positionId);
     Task<IEnumerable<Position>> GetEmployeePositions(int[] checkedPositionsId);
+    Task<Position> GetPositionForSchool(int positionId, int schoolId);
 }
