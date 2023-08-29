ae9d1e57fc1fdb8610a50eeda005239c88e7c780
Віктор Липкань <lipkan02@gmail.com>
2023-08-29 14:07:53 +0300

    add floors handlers

---
+++ SchoolManagement.API/Floors/Handlers/GetFloorByIdHandler.cs
@@ -0,0 +1,24 @@
+﻿using SchoolManagement.Models.Interfaces;
+using SchoolManagement.API.Floors.Dtos;
+using Microsoft.AspNetCore.Mvc;
+
+namespace SchoolManagement.API.Floors.Handlers;
+
+public static class GetFloorByIdHandler
+{
+    public static async Task<IResult> Handle(
+        IFloorRepository repository,
+        [FromRoute] int schoolId,
+        [FromRoute] int floorId)
+    {
+        var floor = await repository.GetFloorAsync(floorId);
+
+        if (floor is null || floor.SchoolId != schoolId)
+        {
+            return Results.NotFound("No such floor found");
+        }
+
+        var floorDto = floor.ToFloorDto();
+        return Results.Json(floorDto);
+    }
+}
---
+++ SchoolManagement.API/Floors/Handlers/UpdateFloorHandler.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SchoolManagement.API.Floors.Dtos;
+using SchoolManagement.Models.Interfaces;
+
+namespace SchoolManagement.API.Floors.Handlers;
+
+public static class UpdateFloorHandler
+{
+    public static async Task<IResult> Handle(
+        IFloorRepository repository, 
+        [FromRoute] int schoolId, 
+        [FromRoute] int floorId,
+        [FromBody] FloorUpdateDto floorDto)
+    {
+        var floor = await repository.GetAsync(floorId);
+
+        if (floor is null)
+        {
+            return Results.NotFound("No such floor found");
+        }
+
+        var floors = await repository
+            .GetAllAsync(f => f.SchoolId == schoolId && f.Number != floor.Number);
+
+        if (floors.Any(f => f.Number == floorDto.Number))
+        {
+            return Results.BadRequest("A floor with this number already exists");
+        }
+
+        if (floorDto.Number != 0)
+        {
+            floor.Number = floorDto.Number;
+        }
+
+        await repository.UpdateAsync(floor);
+        return Results.Ok(floor);
+    }
+}
