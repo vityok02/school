fdea4b64059f52e9d158b877d1e4f533fee1e236
Віктор Липкань <lipkan02@gmail.com>
2023-08-29 14:12:04 +0300

    add students handlers

---
+++ SchoolManagement.API/Students/Handlers/CreateStudentHandler.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SchoolManagement.API.Students.Dtos;
+using SchoolManagement.Models;
+using SchoolManagement.Models.Interfaces;
+
+namespace SchoolManagement.API.Students.Handlers;
+
+public static class CreateStudentHandler
+{
+    public static async Task<IResult> Handle(
+        IRepository<Student> repository,
+        [FromRoute] int schoolId,
+        [FromBody] StudentCreateDto studentDto)
+    {
+        var student = new Student(
+            studentDto.FirstName,
+            studentDto.LastName,
+            studentDto.Age,
+            studentDto.Group)
+        {
+            SchoolId = schoolId
+        };
+
+        await repository.AddAsync(student);
+
+        var createdStudent = student.ToStudentDto();
+        return Results.Created($"/schools/{schoolId}/students/{student.Id}", createdStudent);
+    }
+}
---
+++ SchoolManagement.API/Students/Handlers/GetStudentByIdHandler.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SchoolManagement.API.Students.Dtos;
+using SchoolManagement.Models;
+using SchoolManagement.Models.Interfaces;
+
+namespace SchoolManagement.API.Students.Handlers;
+
+public static class GetStudentByIdHandler
+{
+    public static async Task<IResult> Handle(
+        IRepository<Student> repository,
+        [FromRoute] int schoolId,
+        [FromRoute] int studentId)
+    {
+        var student = (
+            await repository
+            .GetAllAsync(s => s.Id == studentId && s.SchoolId == schoolId))
+            .SingleOrDefault();
+
+        if (student is null)
+        {
+            return Results.NotFound("No such student found");
+        }
+
+        var studentDto = student.ToStudentDto();
+        return Results.Ok(studentDto);
+    }
+}
