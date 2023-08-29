38d57ae62feef1ed9d97b6b44a84cd3c60a1d198
Віктор Липкань <lipkan02@gmail.com>
2023-08-29 14:12:36 +0300

    remove gaps

--- SchoolManagement.API/Program.cs
+++ SchoolManagement.API/Program.cs
@@ -1,9 +1,9 @@
 using SchoolManagement.API.Employees;
-using SchoolManagement.API.Endpoints;
 using SchoolManagement.API.Floors;
 using SchoolManagement.API.Positions;
 using SchoolManagement.API.Rooms;
 using SchoolManagement.API.Schools;
+using SchoolManagement.API.Students;
 using SchoolManagement.Data;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -15,15 +15,10 @@ var app = builder.Build();
 app.MapGet("/", () => "Hello World!");
 
 SchoolsEndpoints.Map(app);
-
 FloorsEndpoints.Map(app);
-
 RoomsEndpoints.Map(app);
-
 EmployeesEndpoints.Map(app);
-
 PositionsEndpoints.Map(app);
-
 StudentsEndpoints.Map(app);
 
 app.Run();
