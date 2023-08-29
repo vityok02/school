2b552198acc5b51ec10ef8ec2078776c75ce29dc
Віктор Липкань <lipkan02@gmail.com>
2023-08-29 14:00:22 +0300

    change method to async

--- school.Models/Interfaces/ISchoolRepository.cs
+++ school.Models/Interfaces/ISchoolRepository.cs
@@ -5,6 +5,6 @@ namespace SchoolManagement.Models.Interfaces;
 public interface ISchoolRepository : IRepository<School>
 {
     Task<School> GetSchoolAsync(int id);
-    IEnumerable<School> GetSchools(Expression<Func<School, bool>> predicate,
+    Task<IEnumerable<School>> GetSchools(Expression<Func<School, bool>> predicate = null!,
         Func<IQueryable<School>, IOrderedQueryable<School>> orderBy = null!);
 }
