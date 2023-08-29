a0e4f476736574eb6559fe0d9bb68a318451911b
Віктор Липкань <lipkan02@gmail.com>
2023-08-29 14:09:16 +0300

    add positions handlers group maps

--- SchoolManagement.API/Positions/PositionsEndpoints.cs
+++ SchoolManagement.API/Positions/PositionsEndpoints.cs
@@ -1,5 +1,4 @@
-﻿using SchoolManagement.API.Positions.Dtos;
-using SchoolManagement.Models.Interfaces;
+﻿using SchoolManagement.API.Positions.Handlers;
 
 namespace SchoolManagement.API.Positions;
 
@@ -7,27 +6,12 @@ public static class PositionsEndpoints
 {
     public static void Map(WebApplication app)
     {
-        app.MapGet("/schools/{id}/positions", 
-            async (IPositionRepository repository, int id) =>
-            {
-                var positions = await repository.GetAllPositions(id);
+        var positionsGroup = app.MapGroup("/schools/{schoolId}/positions");
 
-                return positions.Select(p => new PositionDto(p.Id, p.Name));
-            });
-
-        app.MapGet("/schools/{id}/positions/{positionId}", 
-            async (IPositionRepository repository, int positionId, int id) =>
-            {
-                var position = await repository.GetAsync(positionId);
-                
-
-                //if (position is null || position.Schools)
-            });
-
-        //app.MapPost("/schools/{id}/positions", null!);
-
-        //app.MapPut("/schools/{id}/positions/{positionId}", null!);
-
-        //app.MapDelete("/schools/{id}/positions/{positionId}", null!);
+        positionsGroup.MapGet("/", GetAllPositionsHandler.Handle);
+        positionsGroup.MapGet("/{positionId}", GetPositionByIdHandler.Handle);
+        positionsGroup.MapPost("/", CreatePositionHandler.Handle);
+        positionsGroup.MapPut("/{positionId}", UpdatePositionHandler.Handle);
+        positionsGroup.MapDelete("/{positionId}", DeletePositionHander.Handle);
     }
 }
