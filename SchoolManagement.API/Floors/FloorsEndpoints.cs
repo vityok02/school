b9ea527c0a2e179c8b1fe7f3142984d1a9de1638
Віктор Липкань <lipkan02@gmail.com>
2023-08-29 14:08:33 +0300

    add floors handlers group maps

--- SchoolManagement.API/Floors/FloorsEndpoints.cs
+++ SchoolManagement.API/Floors/FloorsEndpoints.cs
@@ -1,6 +1,4 @@
-﻿using SchoolManagement.API.Floors.Dtos;
-using SchoolManagement.Models;
-using SchoolManagement.Models.Interfaces;
+﻿using SchoolManagement.API.Floors.Handlers;
 
 namespace SchoolManagement.API.Floors;
 
@@ -8,88 +6,12 @@ public static class FloorsEndpoints
 {
     public static void Map(WebApplication app)
     {
-        app.MapGet("/schools/{id}/floors",
-            async (IFloorRepository repository, int id) =>
-            {
-                var floors = await repository.GetFloorsAsync(id);
+        var floorsGroup = app.MapGroup("/schools/{schoolId}/floors");
 
-                return floors.Select(f => f.ToFloorDto());
-            });
-
-        app.MapGet("/schools/{id}/floors/{floorId}",
-            async (IFloorRepository repository, int floorId, int id) =>
-            {
-                var floor = await repository.GetFloorAsync(floorId);
-
-                if (floor is null || floor.SchoolId != id)
-                {
-                    return Results.NotFound("No such floor found");
-                }
-
-                var floorDto = floor.ToFloorDto();
-                return Results.Json(floorDto);
-            });
-
-        app.MapPost("/schools/{id}/floors",
-            async (IFloorRepository repository, int number, int id) =>
-            {
-                var floors = await repository.GetAllAsync(f => f.SchoolId == id);
-
-                if (floors.Any(f => f.Number == number))
-                {
-                    return Results.BadRequest("A floor with this number already exists");
-                }
-
-                Floor floor = new()
-                {
-                    Number = number,
-                    SchoolId = id
-                };
-
-                await repository.AddAsync(floor);
-
-                return Results.Json(floor);
-            });
-
-        app.MapPut("/schools/{id}/floors/{floorId}",
-            async (IFloorRepository repository, int floorId, int number, int id) =>
-            {
-                var floor = await repository.GetAsync(floorId);
-
-                if(floor is null)
-                {
-                    return Results.NotFound("No such floor found");
-                }
-
-                var floors = await repository
-                    .GetAllAsync(f => f.SchoolId == id && f.Number != floor.Number);
-
-                if(floors.Any(f => f.Number == number))
-                {
-                    return Results.BadRequest("A floor with this number already exists");
-                }
-
-                if (number != 0)
-                {
-                    floor.Number = number;
-                }
-
-                await repository.UpdateAsync(floor);
-                return Results.Json(floor);
-            });
-
-        app.MapDelete("/schools/{id}/floors/{floorId}",
-            async (IFloorRepository repository, int floorId, int id) =>
-            {
-                var floor = await repository.GetFloorAsync(floorId);
-
-                if (floor is null || floor.SchoolId != id)
-                {
-                    return Results.NotFound("No such floor found");
-                }
-
-                await repository.DeleteAsync(floor);
-                return Results.Ok();
-            });
+        floorsGroup.MapGet("/", GetAllFloorsHandler.Handle);
+        floorsGroup.MapGet("/{floorId}", GetFloorByIdHandler.Handle);
+        floorsGroup.MapPost("/", CreateFloorHandler.Handle);
+        floorsGroup.MapPut("/{floorId}", UpdateFloorHandler.Handle);
+        floorsGroup.MapDelete("/{floorId}", DeleteFloorHandler.Handle);
     }
 }
