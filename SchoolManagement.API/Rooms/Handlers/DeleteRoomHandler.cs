90f4829c0a2895836f73610ec4b2e2e2456763ec
Віктор Липкань <lipkan02@gmail.com>
2023-08-29 14:11:03 +0300

    add rooms handlers

---
+++ SchoolManagement.API/Rooms/Handlers/DeleteRoomHandler.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SchoolManagement.Models.Interfaces;
+
+namespace SchoolManagement.API.Rooms.Handlers;
+
+public static class DeleteRoomHandler
+{
+    public static async Task<IResult> Handle(
+        IRoomRepository repository,
+        [FromRoute] int schoolId,
+        [FromRoute] int roomId)
+    {
+        var room = await repository.GetRoomAsync(roomId);
+
+        if (room is null || room.Floor.SchoolId != schoolId)
+        {
+            return Results.NotFound("No such room found");
+        }
+
+        await repository.DeleteAsync(room);
+        return Results.Ok();
+    }
+}
--- SchoolManagement.API/Rooms/RoomsEndpoints.cs
+++ SchoolManagement.API/Rooms/RoomsEndpoints.cs
@@ -1,6 +1,4 @@
-﻿using SchoolManagement.API.Rooms.Dtos;
-using SchoolManagement.Models;
-using SchoolManagement.Models.Interfaces;
+﻿using SchoolManagement.API.Rooms.Handlers;
 
 namespace SchoolManagement.API.Rooms;
 
@@ -8,70 +6,12 @@ public static class RoomsEndpoints
 {
     public static void Map(WebApplication app)
     {
-        app.MapGet("/schools/{id}/rooms",
-            async (IRoomRepository repository, int id) =>
-            {
-                var rooms = await repository.GetRoomsAsync(id);
-                return rooms.Select(r => r.ToRoomDto());
-            });
+        var roomsGroup = app.MapGroup("/schools/{schoolId}/rooms");
 
-        app.MapGet("/schools/{id}/rooms/{roomId}",
-            async (IRoomRepository repository, int id, int roomId) =>
-            {
-                var room = await repository.GetRoomAsync(roomId);
-
-                if (room is null || room.Floor.SchoolId != id)
-                {
-                    return Results.NotFound("No such room found");
-                }
-
-                var roomDto = room.ToRoomDto();
-                return Results.Json(roomDto);
-            });
-
-        app.MapPost("/schools/{id}/rooms",
-            async (IRoomRepository repository, int id, RoomAddDto roomDto) =>
-            {
-                var room = new Room()
-                {
-                    Number = roomDto.Number,
-                    Type = roomDto.Types,
-                    FloorId = roomDto.FloorId,
-                };
-
-                await repository.AddAsync(room);
-            });
-
-        app.MapPut("/schools/{id}/rooms/{roomId}",
-            async (IRoomRepository repository, int id, RoomEditDto roomDto, int roomId) =>
-            {
-                var room = await repository.GetRoomAsync(roomId);
-
-                if (room is null || room.Floor.SchoolId != id)
-                {
-                    return Results.NotFound("No such room found");
-                }
-
-                room.Number = roomDto.Number;
-                room.Type = roomDto.Types;
-                room.FloorId = roomDto.FloorId;
-
-                await repository.UpdateAsync(room);
-                return Results.Json(room);
-            });
-
-        app.MapDelete("/schools/{id}/rooms/{roomId}",
-            async (IRoomRepository repository, int roomId, int id) =>
-            {
-                var room = await repository.GetRoomAsync(roomId);
-
-                if(room is null || room.Floor.SchoolId != id)
-                {
-                    return Results.NotFound("No such room found");
-                }
-
-                await repository.DeleteAsync(room);
-                return Results.Ok();
-            });
+        roomsGroup.MapGet("/", GetAllRoomsHandler.Handle);
+        roomsGroup.MapGet("/{roomId}", GetRoomByIdHandler.Handle);
+        roomsGroup.MapPost("/", CreateRoomHandler.Handle);
+        roomsGroup.MapPut("/{roomId}", UpdateRoomHandler.Handle);
+        roomsGroup.MapDelete("/{roomId}", UpdateRoomHandler.Handle);
     }
 }
