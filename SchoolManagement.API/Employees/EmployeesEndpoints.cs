96e946ef45a20508d798019758d6edba8e585e89
Віктор Липкань <lipkan02@gmail.com>
2023-08-29 14:02:54 +0300

    group employees endpoints add handlers

--- SchoolManagement.API/Employees/EmployeesEndpoints.cs
+++ SchoolManagement.API/Employees/EmployeesEndpoints.cs
@@ -1,6 +1,4 @@
-﻿using SchoolManagement.Models;
-using SchoolManagement.Models.Interfaces;
-using SchoolManagement.API.Employees.Dtos;
+﻿using SchoolManagement.API.Employees.Handlers;
 
 namespace SchoolManagement.API.Employees;
 
@@ -8,79 +6,12 @@ public static class EmployeesEndpoints
 {
     public static void Map(WebApplication app)
     {
-        app.MapGet("/schools/{id}/employees", async (IEmployeeRepository repository, int id) =>
-        {
-            var schools = await repository.GetAllAsync(e => e.SchoolId == id);
+        var employeesGroup = app.MapGroup("/schools/{schoolId}/employees");
 
-            return schools.Select(s => s.ToEmployeeDto());
-        });
-
-        app.MapGet("/schools/{id}/employees/{employeeId}",
-            async (IEmployeeRepository repository, int employeeId, int id) =>
-            {
-                var employee = await repository.GetEmployeeWithPositionsAsync(employeeId);
-
-                if (employee is null || employee.SchoolId != id)
-                {
-                    return Results.NotFound("No such employee found");
-                }
-
-                var employeeDto = employee.ToEmployeeDto();
-                return Results.Json(employeeDto);
-            });
-
-        app.MapPost("/schools/{id}/employees",
-            async (
-                IEmployeeRepository employeeRepository,
-                IPositionRepository positionRepository,
-                EmployeeAddDto employeeDto,
-                int id) =>
-            {
-                var employee = new Employee(employeeDto.FirstName!, employeeDto.LastName!, employeeDto.Age)
-                {
-                    Positions = (ICollection<Position>)await positionRepository
-                        .GetEmployeePositions(employeeDto.PositionIds),
-                    SchoolId = id
-                };
-
-                await employeeRepository.AddAsync(employee);
-            });
-
-        app.MapPut("/schools/{id}/employees/{employeeId}",
-            async (
-                IEmployeeRepository employeeRepository,
-                IPositionRepository positionRepository,
-                EmployeeEditDto employeeDto,
-                int employeeId,
-                int id) =>
-            {
-                var employee = await employeeRepository.GetAsync(employeeId);
-
-                if (employee is null || employee.SchoolId != employeeId)
-                {
-                    return Results.NotFound("No such employee found");
-                }
-
-                employee!.UpdateInfo(employeeDto.FirstName, employeeDto.LastName, employeeDto.Age);
-
-                await employeeRepository.UpdateAsync(employee);
-
-                return Results.Json(employee);
-            });
-
-        app.MapDelete("/schools/{id}/employees/{employeeId}",
-            async (IEmployeeRepository repository, int employeeId, int id) =>
-            {
-                var employee = await repository.GetAsync(employeeId);
-
-                if (employee is null || employee.SchoolId != id)
-                {
-                    return Results.NotFound("No such employee found");
-                }
-
-                await repository.DeleteAsync(employee!);
-
-                return Results.Ok();
-            });
+        employeesGroup.MapGet("/", GetAllEmployeesHandler.Handle);
+        employeesGroup.MapGet("/{employeeId}", GetEmployeeByIdHandler.Handle);
+        employeesGroup.MapPost("/", CreateEmployeeHandler.Handle);
+        employeesGroup.MapPut("/{employeeId}", UpdateEmployeeHandler.Handle);
+        employeesGroup.MapDelete("/{employeeId}", DeleteEmployeeHandler.Handle);
     }
 }
