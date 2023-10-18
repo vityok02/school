0bf0a4e27762a3adbcc95607e9a013f064f5b767
lipka <lipkan02@gmail.com>
2023-10-18 19:13:19 +0300

    add filter that validate school id

---
+++ SchoolManagement.API/Filters/SchoolIdExistsFilter.cs
@@ -0,0 +1,33 @@
+﻿using SchoolManagement.Models.Interfaces;
+
+namespace SchoolManagement.API.Filters;
+
+public class SchoolIdExistsFilter : IEndpointFilter
+{
+    private readonly ISchoolRepository _schoolRepository;
+
+    public SchoolIdExistsFilter(ISchoolRepository schoolRepository)
+    {
+        _schoolRepository = schoolRepository;
+    }
+
+    public async ValueTask<object?> InvokeAsync(
+        EndpointFilterInvocationContext efiContext,
+        EndpointFilterDelegate next)
+    {
+        var routes = efiContext.HttpContext.Request.RouteValues;
+
+        var schoolIdFromRoute = routes["schoolId"]!.ToString();
+
+        _ = int.TryParse(schoolIdFromRoute, out int schoolId);
+
+        var school = await _schoolRepository.GetAsync(schoolId);
+
+        if (school is null)
+        {
+            return Results.Problem("School not found", statusCode: StatusCodes.Status404NotFound);
+        }
+
+        return await next(efiContext);
+    }
+}
