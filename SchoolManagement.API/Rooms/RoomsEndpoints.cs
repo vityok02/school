5fd598d4ead0ddcd5f6a682ccb00b6c36ac33a23
lipka <lipkan02@gmail.com>
2023-10-18 19:17:01 +0300

    add swagger add filter

--- SchoolManagement.API/Rooms/RoomsEndpoints.cs
+++ SchoolManagement.API/Rooms/RoomsEndpoints.cs
@@ -1,4 +1,6 @@
-﻿using SchoolManagement.API.Rooms.Handlers;
+﻿using SchoolManagement.API.Filters;
+using SchoolManagement.API.Rooms.Dtos;
+using SchoolManagement.API.Rooms.Handlers;
 
 namespace SchoolManagement.API.Rooms;
 
@@ -6,12 +8,36 @@ public static class RoomsEndpoints
 {
     public static void Map(WebApplication app)
     {
-        var roomsGroup = app.MapGroup("/schools/{schoolId}/rooms");
+        var roomsGroup = app.MapGroup("/schools/{schoolId:int}/rooms")
+            .AddEndpointFilter<SchoolIdExistsFilter>()
+            .WithTags("Rooms group")
+            .WithOpenApi();
 
-        roomsGroup.MapGet("/", GetAllRoomsHandler.Handle);
-        roomsGroup.MapGet("/{roomId}", GetRoomByIdHandler.Handle);
-        roomsGroup.MapPost("/", CreateRoomHandler.Handle);
-        roomsGroup.MapPut("/{roomId}", UpdateRoomHandler.Handle);
-        roomsGroup.MapDelete("/{roomId}", UpdateRoomHandler.Handle);
+        roomsGroup.MapGet("/", GetAllRoomsHandler.Handle)
+            .WithSummary("Get all rooms")
+            .Produces<RoomDto[]>()
+            .Produces(StatusCodes.Status404NotFound);
+
+        roomsGroup.MapGet("/{roomId:int}", GetRoomByIdHandler.Handle)
+            .WithSummary("Get room by id")
+            .Produces<RoomDto>()
+            .Produces(StatusCodes.Status404NotFound);
+
+        roomsGroup.MapPost("/", CreateRoomHandler.Handle)
+            .WithSummary("Create room")
+            .Produces<RoomDto>()
+            .ProducesValidationProblem()
+            .Produces(StatusCodes.Status404NotFound);
+
+        roomsGroup.MapPut("/{roomId:int}", UpdateRoomHandler.Handle)
+            .WithSummary("Update room")
+            .Produces<RoomDto>()
+            .ProducesValidationProblem()
+            .Produces(StatusCodes.Status404NotFound);
+
+        roomsGroup.MapDelete("/{roomId:int}", DeleteRoomHandler.Handle)
+            .WithSummary("Delete room")
+            .Produces(StatusCodes.Status204NoContent)
+            .Produces(StatusCodes.Status404NotFound);
     }
 }
--- SchoolManagement.API/Schools/SchoolsEndpoints.cs
+++ SchoolManagement.API/Schools/SchoolsEndpoints.cs
@@ -1,4 +1,6 @@
-﻿using SchoolManagement.API.Schools.Handlers;
+﻿using SchoolManagement.API.Filters;
+using SchoolManagement.API.Schools.Dtos;
+using SchoolManagement.API.Schools.Handlers;
 
 namespace SchoolManagement.API.Schools;
 
@@ -6,12 +8,38 @@ public static class SchoolsEndpoints
 {
     public static void Map(WebApplication app)
     {
-        var schoolsGroup = app.MapGroup("/schools");
+        var schoolsGroup = app.MapGroup("/schools")
+            .WithTags("Schools group")
+            .WithOpenApi();
 
-        schoolsGroup.MapGet("/", GetAllSchoolsHandler.Handle);
-        schoolsGroup.MapGet("/{schoolId}", GetSchoolByIdHandler.Handle);
-        schoolsGroup.MapPost("/", CreateSchoolHandler.Handle);
-        schoolsGroup.MapPut("/{schoolId}", UpdateSchoolHandler.Handle);
-        schoolsGroup.MapDelete("/{schoolId}", DeleteSchoolHandler.Handle);
+        schoolsGroup.MapGet("/", GetAllSchoolsHandler.Handle)
+            .WithSummary("Get all schools")
+            .Produces<SchoolItemDto[]>()
+            .Produces(StatusCodes.Status404NotFound);
+
+        schoolsGroup.MapGet("/{schoolId:int}", GetSchoolByIdHandler.Handle)
+            .AddEndpointFilter<SchoolIdExistsFilter>()
+            .WithSummary("Get school by id")
+            .Produces<SchoolDetailsDto>()
+            .Produces(StatusCodes.Status404NotFound);
+
+        schoolsGroup.MapPost("/", CreateSchoolHandler.Handle)
+            .WithSummary("Create school")
+            .Produces<SchoolDetailsDto>()
+            .ProducesValidationProblem()
+            .Produces(StatusCodes.Status404NotFound);
+
+        schoolsGroup.MapPut("/{schoolId:int}", UpdateSchoolHandler.Handle)
+            .AddEndpointFilter<SchoolIdExistsFilter>()
+            .WithSummary("Update school")
+            .Produces<SchoolDetailsDto>()
+            .ProducesValidationProblem()
+            .Produces(StatusCodes.Status404NotFound);
+
+        schoolsGroup.MapDelete("/{schoolId:int}", DeleteSchoolHandler.Handle)
+            .AddEndpointFilter<SchoolIdExistsFilter>()
+            .WithSummary("Delete school")
+            .Produces(StatusCodes.Status204NoContent)
+            .Produces(StatusCodes.Status404NotFound);
     }
 }
