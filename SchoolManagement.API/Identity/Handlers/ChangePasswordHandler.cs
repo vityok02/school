23b9bffbab7afecd193297ddb1e460d96e571e44
lipka <lipkan02@gmail.com>
2023-12-12 11:47:59 +0200

    add ChangePasswordHandler

---
+++ SchoolManagement.API/Identity/Handlers/ChangePasswordHandler.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using SchoolManagement.API.Identity.Requests;
+
+namespace SchoolManagement.API.Identity.Handlers;
+
+public static class ChangePasswordHandler
+{
+    public static async Task<IResult> Handle(
+        HttpContext context,
+        [FromBody] ChangePasswordRequest request,
+        UserManager<IdentityUser<int>> userManager)
+    {
+        var user = await userManager.FindByNameAsync(context.User.Identity.Name);
+
+        if (user is null)
+        {
+            return Results.Unauthorized();
+        }
+
+        if (request.NewPassword != request.ConfirmedPassword)
+        {
+            return Results.Problem();
+        }
+
+        var result = await userManager.ChangePasswordAsync(user, request.Password, request.NewPassword);
+
+        if (!result.Succeeded)
+        {
+            return Results.Problem(string.Join(";", result.Errors.Select(e => $"{e.Code}: {e.Description}")));
+        }
+
+        return Results.Ok("The password has been changed");
+    }
+}
