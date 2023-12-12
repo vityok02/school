9b16e7ed21e2df6be95e2f9a8264530612b5bb40
lipka <lipkan02@gmail.com>
2023-12-12 12:33:20 +0200

    add Authorization options

--- SchoolManagement.API/AuthOptions.cs
+++ SchoolManagement.API/AuthOptions.cs
@@ -1,11 +1,13 @@
 ﻿using Microsoft.IdentityModel.Tokens;
 using System.Text;
 
+namespace SchoolManagement.API;
+
 public class AuthOptions
 {
-    public const string ISSUER = "MyAuthServer"; // издатель токена
-    public const string AUDIENCE = "MyAuthClient"; // потребитель токена
-    const string KEY = "mysupersecret_secretkey!123";   // ключ для шифрации
+    public const string ISSUER = "MyAuthServer";
+    public const string AUDIENCE = "MyAuthClient";
+    const string KEY = "mysupersecret_secretkey!123";
     public static SymmetricSecurityKey GetSymmetricSecurityKey() =>
-        new SymmetricSecurityKey(Encoding.UTF8.GetBytes(KEY));
+        new(Encoding.UTF8.GetBytes(KEY));
 }
