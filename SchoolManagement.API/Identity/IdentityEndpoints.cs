947aa7c73321693584c6420becd37fd969dc13d1
lipka <lipkan02@gmail.com>
2023-12-12 12:34:42 +0200

    Add authentication endpoints

--- SchoolManagement.API/Identity/IdentityEndpoints.cs
+++ SchoolManagement.API/Identity/IdentityEndpoints.cs
@@ -6,12 +6,27 @@ public static class IdentityEndpoints
 {
     public static void Map(WebApplication app)
     {
-        var identityGroup = app.MapGroup("/");
+        var identityGroup = app.MapGroup("/")
+            .WithTags("Identity")
+            .WithOpenApi();
 
-        identityGroup.MapPost("/reg", RegistrationHandler.Handle);
+        identityGroup.MapPost("/reg", RegistrationHandler.Handle)
+            .WithSummary("Register");
 
-        identityGroup.MapPost("/log", LoginHandler.Handle);
+        identityGroup.MapPost("/log", LoginHandler.Handle)
+            .WithSummary("Login");
 
-        identityGroup.MapPost("/claims", AddClaimsHandler.Handle);
+        identityGroup.MapPost("/claims", AddClaimsHandler.Handle)
+            .WithSummary("Add claims");
+
+        identityGroup.MapPost("/reset-password", ResetPasswordHandler.Handle)
+            .WithSummary("Reset password");
+
+        identityGroup.MapPost("/change-password", ChangePasswordHandler.Handle)
+            .WithSummary("Change password")
+            .RequireAuthorization();
+
+        identityGroup.MapPost("/set-password", SetPasswordHandler.Handle)
+            .WithSummary("Set password");
     }
 }
