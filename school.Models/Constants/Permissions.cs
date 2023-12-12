9a57b21dd845b2f75d827933ada037402bdd2b55
lipka <lipkan02@gmail.com>
2023-12-12 10:17:40 +0200

    add permissions constants

---
+++ school.Models/Constants/Permissions.cs
@@ -0,0 +1,14 @@
+﻿namespace SchoolManagement.Models.Constants;
+
+public static class Permissions
+{
+    public const string CanManageSchools = nameof(CanManageSchools);
+    public const string CanManageSchoolUsers = nameof(CanManageSchoolUsers);
+    public const string CanManageEmployees = nameof(CanManageEmployees);
+    public const string CanManageFloors = nameof(CanManageFloors);
+    public const string CanManageRooms = nameof(CanManageRooms);
+    public const string CanManagePositions = nameof(CanManagePositions);
+    public const string CanManageSchoolPositions = nameof(CanManageSchoolPositions);
+    public const string CanManageStudents = nameof(CanManageStudents);
+    public const string CanViewInfo = nameof(CanViewInfo);
+}
