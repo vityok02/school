cb8c15f5d1b314508dd563dddc67d76c1ae3728b
lipka <lipkan02@gmail.com>
2023-12-12 12:31:29 +0200

    add: Implement IsAdmin method for user authentication

---
+++ SchoolManagement.API/Identity/Requests/AuthRequest.cs
@@ -0,0 +1,8 @@
+﻿using Azure.Core;
+
+namespace SchoolManagement.API.Identity.Requests;
+
+public record AuthRequest(string UserName, string Password)
+{
+    public bool IsAdmin() => UserName == "admin" && Password == "Pass@word1";
+}
