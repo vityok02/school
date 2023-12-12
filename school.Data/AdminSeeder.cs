7b1bf022355329b8047c62e0de8de5dbc2ecc8d8
lipka <lipkan02@gmail.com>
2023-12-12 09:44:31 +0200

    Add admin seeder

---
+++ school.Data/AdminSeeder.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Identity;
+using SchoolManagement.Models.Constants;
+
+namespace SchoolManagement.Data;
+
+public class AdminSeeder
+{
+    public static async Task SeedAdmin(UserManager<IdentityUser<int>> userManager)
+    {
+        if (await userManager.FindByNameAsync("admin") is not null)
+        {
+            return;
+        }
+
+        IdentityUser<int> admin = new()
+        {
+            UserName = "admin",
+            LockoutEnabled = false
+        };
+
+        var result = await userManager.CreateAsync(admin, "Pass@word1");
+
+        if (!result.Succeeded)
+        {
+            return;
+        }
+
+        await userManager.AddClaimsAsync(admin, ClaimGroups.SystemAdminPermissions);
+    }
+}
