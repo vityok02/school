686be1622f876d54c5a4b4482d4e9844b93d15ae
lipka <lipkan02@gmail.com>
2023-12-12 12:23:21 +0200

    refactor

--- SchoolManagement.API/Identity/Handlers/AddClaimsHandler.cs
+++ SchoolManagement.API/Identity/Handlers/AddClaimsHandler.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
-using SchoolManagement.API.Constants;
+using SchoolManagement.Models.ClaimGroups;
+using SchoolManagement.Models.Constants;
 using System.Security.Claims;
 
 namespace SchoolManagement.API.Identity.Handlers;
@@ -19,61 +20,28 @@ class AddClaimsHandler
             return Results.Unauthorized();
         }
 
-        foreach(var claim in claimRequest.Claims)
-        {
-            await userManager.AddClaimAsync(user, new Claim(claim.Key, claim.Value));
-        }
+        var userClaims = await userManager.GetClaimsAsync(user);
 
-        return Results.Empty;
-
-        //SysAdmin can give SchoolAdmin only
-        if (context.User.HasClaim(ClaimNames.Permissions, Permissions.CanManageSchoolAdmin))
+        if (context.User.HasClaim(ClaimNames.Permissions, Permissions.CanManageSchools))
         {
-            foreach(var claim in claimRequest.Claims)
+            foreach (var claim in SchoolAdminClaims.Claims)
             {
-                await userManager.AddClaimAsync(user, new Claim(ClaimNames.Permissions, ClaimValues.SchoolAdmin));
+                if(!userClaims.Any(c => c.Value == claim.Value))
+                {
+                    await userManager.AddClaimAsync(user, claim);
+                }
             }
         }
 
-        //SchoolAdmin can give Employee only
+        if (context.User.HasClaim(ClaimNames.Permissions, Permissions.CanManageEmployees))
+        {
+            foreach (var claim in claimRequest.Claims)
+            {
+                await userManager.AddClaimAsync(user, new Claim(ClaimNames.Permissions, Permissions.CanManageSchoolUsers));
+            }
+        }
 
-        //var admin = context.User.FindAll(c => c.Type == ClaimNames.Admin);
-
-        //var result = await AssignClaims(ClaimValues.SchoolAdmin, ClaimNames.Employee);
-
-        //if (result != Results.Empty)
-        //{
-        //    return result;
-        //}
-
-        //result = await AssignClaims(ClaimValues.SystemAdmin, ClaimNames.Admin);
-
-        //if (result != Results.Empty)
-        //{
-        //    return result;
-        //}
-
-        //return Results.Ok();
-
-        //async Task<IResult> AssignClaims(string adminType, string type)
-        //{
-        //    if (!admin.Any(ac => ac.Value == adminType))
-        //    {
-        //        return Results.Empty;
-        //    }
-
-        //    foreach (var claim in claimRequest.Claims.Where(c => c.Key == type))
-        //    {
-        //        var result = await userManager.AddClaimAsync(user, new Claim(claim.Key, claim.Value));
-
-        //        if (!result.Succeeded)
-        //        {
-        //            return Results.Problem(string.Join(";", result.Errors.Select(e => $"{e.Code}: {e.Description}")));
-        //        }
-        //    }
-
-        //    return Results.Empty;
-        //}
+        return Results.Empty;
     }
 }
 
